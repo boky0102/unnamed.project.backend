5e34969db29e206645084e6d40dd27ea41d6f07a
boky0102 <bivankovi@protonmail.com>
2024-03-26 21:01:49 +0100

    installed express, wrote basic server script listening on port 3000

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("running");
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("Route good");
+})
+
+app.listen(3000 ,() => {
+    console.log("SERVER IS RUNNING ON PORT 3000")
+})
