eda0f50a9f932e314f3353837de13211c3583378
Nikos Andriopoulos <nickos2014andriopoulos@gmail.com>
2024-03-27 19:52:21 +0200

    Added types

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,7 +1,8 @@
 import { pool } from "./db.services"
+import { User } from "../Types/userType";
 
 // service for getting data for user from db
-export const getAllUsers = async () => {
+export const getAllUsers = async (): Promise<User[]> => {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM Users');
     const users = result.rows;
