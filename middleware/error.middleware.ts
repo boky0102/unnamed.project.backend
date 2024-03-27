76684eb8d2b00fcb82ebed56f09e764546fb2925
boky0102 <bivankovi@protonmail.com>
2024-03-27 19:55:33 +0100

    added error handling middleware and HttpException class

---
+++ middleware/error.middleware.ts
@@ -0,0 +1,40 @@
+import { Response, Request, NextFunction } from "express";
+import { HttpException } from "../Types/error";
+
+/*  this error handling middleware will be called first
+    when function next(err) is called from controller.
+    it prints error stack trace and calles passes error to next
+    error handling middleware
+*/
+export const logErrors = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    next(err);
+}
+
+
+/*
+    this is error middleware that will be called second in
+    the case of error being instance of HttpException.
+    HttpException extends error with status code and therefore
+    can be used as response generator in the case of a error.
+    If error is not instance of HttpException handler is passing
+    error to default error handler with function next(err)
+*/
+export const httpExceptionHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof HttpException){
+        res.status(err.status).send(err.message);
+    }else{
+        next(err);
+    }
+}
+
+/*
+    this is a default error handler which will be called in by
+    all error throws which are not of a HttpException type.
+    It responds with status code 500 and gives message Internal server error.
+*/
+export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).send("Internal server error");
+}
+
+
