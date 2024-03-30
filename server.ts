c466616f148c10d19b5ccd23b4a79ab6085797ac
boky0102 <bivankovi@protonmail.com>
2024-03-30 13:46:56 +0100

    Created function for setting up database on server startup, modified server to wait for database initialization before running

--- server.ts
+++ server.ts
@@ -3,22 +3,30 @@ import express from 'express';
 import { Request, Response } from 'express';
 import { userRouter } from './routes/users';
 import { defaultErrorHandler, httpExceptionHandler, logErrors } from './middleware/error.middleware';
+import { initializeDB } from './services/db.services';
 
+initializeDB()
+    .then(() => {
+        const app = express();
 
-const app = express();
+        app.use(express.json()); // Parse incoming requests data
 
-app.use(express.json()); // Parse incoming requests data
+        app.use(logErrors); // middleware for logging errors
+        app.use(httpExceptionHandler); // middleware for handling http errors
+        app.use(defaultErrorHandler); // middleware for catching all other errors
 
-app.use(logErrors); // middleware for logging errors
-app.use(httpExceptionHandler); // middleware for handling http errors
-app.use(defaultErrorHandler); // middleware for catching all other errors
+        app.use("/users", userRouter); //endpoint specified
 
-app.use("/users", userRouter); //endpoint specified
+        app.get("/", (req: Request, res: Response) => {
+            res.status(200).send("Route good");
+        })
 
-app.get("/", (req: Request, res: Response) => {
-    res.status(200).send("Route good");
-})
+        app.listen(3000, () => {
+            console.log("server is running on 3000");
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    })
 
-app.listen(3000, () => {
-    console.log("server is running on 3000");
-})
--- services/db.services.ts
+++ services/db.services.ts
@@ -1,6 +1,5 @@
-import { Pool } from 'pg';
 require('dotenv').config();
-
+import { Pool, Client, Query, QueryResult } from 'pg';
 
 // single pool instance that will be shared across all services
 export const pool = new Pool({
@@ -10,3 +9,47 @@ export const pool = new Pool({
     password: process.env.DB_PASSWORD,
     port: Number(process.env.DB_PORT)
 });
+
+/*
+    This is a function for initializing database. It will
+    ease setup of db for people who still don't have it
+    running. Moreover, it will ensure that all collaborators 
+    have the same database schema. 
+*/
+export const initializeDB = async () => {
+    try{
+
+        const database = new Client({
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: Number(process.env.DB_PORT)
+        })   
+
+        await database.connect();
+        
+        await database.query("DROP TABLE IF EXISTS Users");  // deleting table to make adding new fields easier
+
+        // creates user table in the database
+        await database.query(`
+        CREATE TABLE Users(
+            uid TEXT PRIMARY KEY NOT NULL,
+            username TEXT NOT NULL,
+            auth_token TEXT,
+            field_of_study TEXT,
+            contributions INT
+        )`);
+        
+        await database.end();
+
+
+        console.log("DB initialized successfully");
+
+    }catch(error){
+        console.log("Failed to create db, please check that you have .env file in project and that you have local postgres db running");
+        console.log(error)
+        process.exit(1);  //terminates node process (shuts down server if db is not created successfully)
+    }
+}
+
