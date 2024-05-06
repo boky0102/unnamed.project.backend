2b3baf9e1518fee29109e2f663c7815c50b516b1
boky0102 <bivankovi@protonmail.com>
2024-05-06 20:30:00 +0200

    Fixed db service only initializing local database

--- services/db.services.ts
+++ services/db.services.ts
@@ -37,13 +37,27 @@ if(process.env.DB_LOCATION === "local"){
 export const initializeDB = async () => {
     try{
 
-        const database = new Client({
-            user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASSWORD,
-            port: Number(process.env.DB_PORT)
-        })
+
+        let database: Client;
+
+
+        if(process.env.DB_LOCATION === "local"){
+            database = new Client({
+                user: process.env.DB_USER,
+                host: process.env.DB_HOST,
+                database: process.env.DB_NAME,
+                password: process.env.DB_PASSWORD,
+                port: Number(process.env.DB_PORT)
+            })
+        } else{
+            database = new Client({
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            })
+        }
+        
 
         await database.connect();
 
