9ef4cdb456c40d6430b067e423acf7c45f5958d9
boky0102 <bivankovi@protonmail.com>
2024-06-17 22:01:53 +0200

    Created question service for saving multi choice questions to database

--- Types/question.types.ts
+++ Types/question.types.ts
@@ -5,12 +5,15 @@ type OpenQuestion = {
     rating?: number
 }
 
+
+//POST REQUEST DATA
 type OpenQuestionData = {
     question: string,
     rating?: number,
     sid: number
 }
 
+//DATABASE DATA
 type Question = {
     qid: number,
     uid: string,
@@ -18,3 +21,28 @@ type Question = {
     oqid?: number,
     cqid?: number
 }
+
+
+//DATABASE DATA
+type ChoiceQuestion = {
+    cqid: number,
+    question: string,
+    answer1: string,
+    answer2: string,
+    answer3: string,
+    answer4: string,
+    solution: number,
+    rating?: number,
+    sid: number
+}
+
+// POST REQUEST DATA
+type ChoiceQuestionData = {
+    question: string,
+    answer1: string,
+    answer2: string,
+    answer3: string,
+    answer4: string,
+    solution: number,
+    sid: number
+};
